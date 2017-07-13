5287b8fd5118843f87146612a55cb6c1cb9eeceb
Madeline Silva <mad.silva.ic@gmail.com>
2017-07-13 11:28:49 -0500

    added url field to character model, changed character model image upload name

--- charbattler/admin.py
+++ charbattler/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Character, Matchup
+from .models import Character, Matchup, Origin
 
 @admin.register(Character)
 class CharacterAdmin(admin.ModelAdmin):
-    fields = (('name', 'bio'), ('total_wins', 'total_losses'), 'image')
+    fields = (('name', 'origin', 'bio', 'more_info_url'), ('total_wins', 'total_losses'), 'image')
 
 admin.site.register(Matchup)
+admin.site.register(Origin)
--- charbattler/models.py
+++ charbattler/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.core.exceptions import ValidationError
+from django.template.defaultfilters import slugify
 import random
 
 
@@ -8,24 +10,53 @@ class Origin(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
 
+    def __str__(self):
+        return self.name
+
+    def get_name_slug(self):
+        return slugify(self.name)
 
 class RandomManager(models.Manager):
     # Returns a random model instance
     def random(self):
         return self.all()[random.randint(0, self.all().count() - 1)]
 
+def character_image_path(instance, filename):
+    ext = filename.split('.')[-1]
+    return 'character_images/{0}/{1}.{2}'.format(slugify(instance.origin), slugify(instance.name), ext)
 
-# TODO: add image file field
+# TODO: override validation method
+# TODO: name and origin unique together
 class Character(models.Model):
+    def get_name_slug(self):
+        return slugify(self.name)
+
+    # the name of the character, displayed in the battler, in lists, etc.
     name = models.CharField(max_length=200)
-    bio = models.TextField()
-    origin = models.ForeignKey(Origin, null=True, blank=True)
+    # the original media of the character
+    origin = models.ForeignKey(Origin)
+    # image to be displayed in the battler, on detail page, etc.
+    # stored with the name "name_origin"
+    image = models.ImageField(upload_to=character_image_path)
+
+    # only one of the two fields below is required
+    # a short description of the character
+    bio = models.TextField(null=True, blank=True)
+    # a url to an outside page providing more detailed info about the character
+    more_info_url = models.URLField(null=True, blank=True)
+
+    # stats for the character
     total_wins = models.IntegerField(default=0)
     total_losses = models.IntegerField(default=0)
-    image = models.ImageField(upload_to='character_pictures/', null=True, blank=True)
 
     objects = RandomManager()
 
+    # override
+    def clean(self):
+        if self.bio is None and self.more_info_url is None:
+            raise ValidationError('Character must have bio and/or info link.')
+
+    # override
     def __str__(self):
         return self.name
 
