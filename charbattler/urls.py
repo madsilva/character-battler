d794c55f9919d6f54f1856b83b02ec4ac4df085e
Madeline Silva <mad.silva.ic@gmail.com>
2017-07-23 16:04:25 -0500

    Puts the matchup form into a separate file for use on multiple pages. Creates a Jojo battler url/view/template.

--- charbattler/urls.py
+++ charbattler/urls.py
@@ -5,13 +5,14 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^jojo_battler/$', views.jojo_battler, name='jojo-battler'),
     url(r'^vote/$', views.vote, name='vote'),
     url(r'^overall_rankings/$', views.overall_rankings, name='overall-rankings'),
+    url(r'^origins/$', views.OriginListView.as_view(), name='origin-list'),
+    url(r'^origins/(?P<pk>[0-9]+)$', views.OriginDetailView.as_view(), name="origin-detail"),
     url(r'^characters/$', views.CharacterListView.as_view(), name='character-list'),
     url(r'^characters/(?P<pk>[0-9]+)$', views.CharacterDetailView.as_view(), name='character-detail'),
     url(r'^matchups/(?P<pk>[0-9]+)$', views.MatchupDetailView.as_view(), name='matchup-detail'),
-    url(r'^origins/(?P<pk>[0-9]+)$', views.OriginDetailView.as_view(), name="origin-detail"),
-    url(r'^origins/$', views.OriginListView.as_view(), name='origin-list'),
     url(r'^about/$', TemplateView.as_view(template_name='charbattler/about.html'), name='about'),
     url(r'^rating_system/$', TemplateView.as_view(template_name='charbattler/obscurity_rating_system.html'), name='rating-system'),
 ]
--- charbattler/views.py
+++ charbattler/views.py
@@ -10,23 +10,42 @@ from .models import Character, Matchup, Origin
 
 
 def index(request):
-    origin = Origin.objects.get(name='Real Life')
-    #matchup = Matchup.objects.random(Matchup.objects.filter(first_character__origin=origin))
     matchup = Matchup.objects.random(Matchup.objects.filter(obscurity_rating__lte=1, first_character__isHidden=False, second_character__isHidden=False))
     mix_up_val = random.randint(0, 1)
     return render(request, 'charbattler/index.html', context={'matchup': matchup, 'mix_up_val': mix_up_val})
 
 
 def vote(request):
+    '''
+    context variables sent by matchup form:
+    matchup: the pk of the matchup that's being voted on
+    winner: the pk of the character in the matchup that was voted to win
+    redirect: the name of the url to redirect to after the vote is processed (almost certainly the page that sent the vote request)
+    '''
     matchup = Matchup.objects.get(pk=request.POST['matchup'])
     matchup.update_wins(request.POST['winner'])
     messages.add_message(request, messages.INFO, matchup.pk)
-    return HttpResponseRedirect(reverse('index'))
+    return HttpResponseRedirect(reverse(request.POST['redirect']))
+
+
+def jojo_battler(request):
+    origin = Origin.objects.get(name='Jojo\'s Bizarre Adventure')
+    matchup = Matchup.objects.random(Matchup.objects.filter(first_character__origin=origin, second_character__origin=origin))
+    mix_up_val = random.randint(0, 1)
+    return render(request, 'charbattler/jojo_battler.html', context={'matchup': matchup, 'mix_up_val': mix_up_val})
 
 
 def overall_rankings(request):
     top10 = Character.objects.order_by('-total_wins')[:10]
     return render(request, 'charbattler/overall_rankings.html', context={'top10': top10})
+
+
+class OriginListView(generic.ListView):
+    model = Origin
+
+
+class OriginDetailView(generic.DetailView):
+    model = Origin
 
 
 class CharacterListView(generic.ListView):
@@ -36,14 +55,6 @@ class CharacterListView(generic.ListView):
 
 class CharacterDetailView(generic.DetailView):
     model = Character
-
-
-class OriginListView(generic.ListView):
-    model = Origin
-
-
-class OriginDetailView(generic.DetailView):
-    model = Origin
 
 
 class MatchupDetailView(generic.DetailView):
