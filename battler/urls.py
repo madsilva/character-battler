d73b9ecd2f3b5cc1045fbc359a29007477fc2012
Madeline Silva <mad.silva.ic@gmail.com>
2017-07-24 12:26:01 -0500

    Changes battler/urls.py to direct all urls to charbattler/urls.py except admin. Adds images and details to matchup detail page. Adds footer and tweaks CSS.

--- battler/urls.py
+++ battler/urls.py
@@ -17,11 +17,9 @@ from django.conf import settings
 from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.views.generic import RedirectView
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^battler/', include('charbattler.urls')),
-    url(r'^$', RedirectView.as_view(url='/battler/', permanent=True)),
+    url(r'^', include('charbattler.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
